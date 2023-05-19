971e73e843fb022f46cb0909b8f3ffbee8db3df1
Alexey Khalin <alexeykhalin82@gmail.com>
2023-05-19 11:29:28 +0500

    Initial commit

---
+++ Entity JSON Config using MVVM/Models/EntityDB.cs
@@ -0,0 +1,75 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace Entity_JSON_Config_using_MVVM.Models
+{
+    public class ApplicationContext : DbContext
+    {
+        //We have DefaultConnection in our JSON file
+        //YOU WILL NEED TO DOWNLOAD Microsoft.Extensions.Configuration.Json NUGET PACKAGE
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            var MyConfig = new ConfigurationBuilder();
+            MyConfig.AddJsonFile("Models/DBConfig.json", optional: false).Build();
+            var config = MyConfig.Build();
+            string connectionString = config.GetConnectionString("DefaultConnection");
+            var options = optionsBuilder.UseSqlite(connectionString).Options;
+        }
+
+        public DbSet<SAVE> Save { get; set; } = null!;
+        public ApplicationContext() => Database.EnsureCreated();
+
+    }
+    public class SAVE
+    {
+        public int Id { get; set; }
+        public string? Data { get; set; }
+    }
+
+    class EntityDB
+    {
+        private string data;
+        public string Data
+        {
+            get { return data; }
+            set
+            {
+                data = value;
+            }
+        }
+        public static void ClearDB()
+        {
+            using (ApplicationContext db = new ApplicationContext())
+            {
+                List<SAVE> ToDelete = db.Save.ToList();
+                db.Save.RemoveRange(ToDelete);
+                db.Database.ExecuteSqlRaw("DELETE FROM sqlite_sequence WHERE name = 'Save'; INSERT INTO sqlite_sequence (name, seq) VALUES ('Save', 0)");
+                db.SaveChanges();
+            }
+        }
+
+        public string? SaveData(string DataToSaveToDB, string DataToReturn)
+        {
+            using var context = new ApplicationContext();
+            var NewSave = new SAVE //This war will save data in DB with SAVE class
+            {
+                Data = DataToSaveToDB
+            };
+            context.Save.Add(NewSave);
+            context.SaveChanges();
+            using (ApplicationContext db = new ApplicationContext())
+            {
+                var saves = db.Save.ToList();
+                DataToReturn = null;
+            foreach (SAVE S in saves)
+            {
+                DataToReturn += $"{S.Id} - {S.Data}\n";
+                //You can also use array if you wish
+            }
+        }
+            return DataToReturn;
+        }
+    }
+}
---
+++ Entity JSON Config using MVVM/ViewModels/ViewModel.cs
@@ -0,0 +1,104 @@
+﻿using Entity_JSON_Config_using_MVVM.Models;
+using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.ComponentModel;
+using System.Linq;
+using System.Runtime.CompilerServices;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Input;
+
+namespace Entity_JSON_Config_using_MVVM.ViewModels
+{
+    class ViewModel : INotifyPropertyChanged
+    {
+        private string enteredData;
+
+        public string EnteredData
+        {
+            get { return enteredData; }
+            set { enteredData = value;
+                OnPropertyChanged("EnteredData");
+            }
+        }
+
+        private string dataToOutput;
+
+        public string DataToOutput
+        {
+            get { return dataToOutput; }
+            set { dataToOutput = value;
+                OnPropertyChanged("DataToOutput");
+            }
+        }
+
+        private RelayCommand addToDB;
+        public RelayCommand AddToDB
+        {
+            get
+            {
+                return addToDB ??
+                  (addToDB = new RelayCommand(obj =>
+                  {
+                      var EntityAdd = new EntityDB();
+                      DataToOutput = EntityAdd.SaveData(EnteredData, DataToOutput);
+                      OnPropertyChanged("EnteredData");
+                  }));
+            }
+        }
+
+        private RelayCommand clearDB;
+        public RelayCommand ClearDB
+        {
+            get
+            {
+                return clearDB ??
+                  (clearDB = new RelayCommand(obj =>
+                  {
+                      EntityDB.ClearDB();
+                      DataToOutput = null; 
+                      OnPropertyChanged("DataToOutput");
+                      OnPropertyChanged("EnteredData");
+                  }));
+            }
+        }
+        public event PropertyChangedEventHandler PropertyChanged;
+        public void OnPropertyChanged([CallerMemberName] string prop = "")
+        {
+            if (PropertyChanged != null)
+                PropertyChanged(this, new PropertyChangedEventArgs(prop));
+        }
+    }
+
+
+
+    public class RelayCommand : ICommand
+    {
+        private Action<object> execute;
+        private Func<object, bool> canExecute;
+
+        public event EventHandler CanExecuteChanged
+        {
+            add { CommandManager.RequerySuggested += value; }
+            remove { CommandManager.RequerySuggested -= value; }
+        }
+
+        public RelayCommand(Action<object> execute, Func<object, bool> canExecute = null)
+        {
+            this.execute = execute;
+            this.canExecute = canExecute;
+        }
+
+        public bool CanExecute(object parameter)
+        {
+            return this.canExecute == null || this.canExecute(parameter);
+        }
+
+        public void Execute(object parameter)
+        {
+            this.execute(parameter);
+        }
+    }
+}
